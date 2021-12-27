272bb5ae56fa21337db279383432bfb432b7617b
AndresRodas <56941654+AndresRodas@users.noreply.github.com>
2021-12-27 12:54:16 -0600

    set runtime

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, render_template
 from flask.json import jsonify
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_folder='client/build', static_url_path='')
+app = Flask(__name__, static_folder='/static/', static_url_path='/static/')
 CORS(app)
 
 @app.route('/', methods=['GET'])
